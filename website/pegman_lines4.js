2f5aad948b6172e70fd25280cdd614774e552da0
Kaylee Kimbrough <khkimbrough@gmail.com>
2016-08-05 11:51:07 -0500

    updating website folder

--- website/pegman_lines4.js
+++ website/pegman_lines4.js
@@ -93,12 +93,17 @@ function addLatLng(event) {
       label: 'B',
       //animation: google.maps.Animation.DROP
     });
-    var contentString_B = '<h5>ut tower</h5>';
+ var contentString_B = "Success! You finished this level! ";
+    var nextLevel = "Next Level";
+    var contentstring = contentString_B + nextLevel.link("A2B_game5.html");
     var infowindow_B = new google.maps.InfoWindow({
-      content: contentString_B
+      content: contentstring
     });
     pointB.addListener('click', info_B);
   }
+   if (google.maps.geometry.spherical.computeDistanceBetween(pointB.getPosition(), event.latLng) < 100) {
+    infowindow_B.open(map, pointB);
+  } 
 
   function info_B() {
     infowindow_B.open(map, pointB);
